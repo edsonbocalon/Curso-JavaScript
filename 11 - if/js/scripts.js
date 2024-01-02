745da2f078df818522b9856e4ba9497310451d55
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2024-01-02 18:25:46 -0300

    Add files via upload

---
+++ 11 - if/js/scripts.js
@@ -0,0 +1,14 @@
+var idade = 19;
+var idadeMinima = 18;
+
+console.log("Antes do if");
+
+if(idade > 18) {
+    console.log("Pode fazer a carteira de habilitação");
+}
+
+if(idade > 15){
+    console.log("Precisa esperar 3 anos ainda");
+}
+
+console.log("Depois do if");
