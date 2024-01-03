169719c5dda94df393b62a5b05ed8740de2ba4fb
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2024-01-03 15:29:38 -0300

    Add files via upload

---
+++ 12 - operador de comparação parte 1/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 15;
+var passuiCarro = 1;
+
+if(idade >= 18){
+    console.log("O usuário pode fazer a carteira");
+}
+
+if(idade <= 17){
+    console.log("O usuário não pdoe fazer a carteira de habilitação");
+}
+
+if(possuiCarro){
+    console.log("O usuário já pdoe andar de carro"); 
+}
+
+var nome = "Edson";
+
+if(nome == "Edson"){
+    console.log("O meu nome é Edson");
+}
+
+if(nome != "Edson"){
+    console.log("O nome não é Edson");
+}
+
+if(20 > 10){
+    console.log("Passou");
+}
+
+if(10 < 20){
+    console.log("Passou 2");
+}
