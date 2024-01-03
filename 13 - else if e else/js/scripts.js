0df752e6677bb37f1e22b716677d372e1f9b1f36
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2024-01-03 14:23:11 -0300

    Add files via upload

---
+++ 13 - else if e else/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Edson";
+
+if(nome == "Edinho"){
+    console.log("O nome dele é Edinho");
+} else if(nome == "Edosn") {
+    console.log("O nome é Edson");
+} else if(nome == "Xavier"){
+    console.log("O nome é Xavier");
+} else{
+    console.log("Ele possui outro nome!");
+} 
+
+var idade = 19;
+
+if(idade > 20){
+    console.log("Ele pode entrar na festa!");
+} else if(idade >= 18){
+    console.log("Ele só pode entrar com autorização")
+}
+
+if(nome == "Edson") {
+  console.log("teste");
+} else {
+  console.log("testando");
+}
