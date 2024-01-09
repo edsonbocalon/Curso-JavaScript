35102dc1b2a54226bbd3617a42c1d0e03a44bf39
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2024-01-09 17:16:21 -0300

    Add files via upload

---
+++ 22 - funções/js/scripts.js
@@ -0,0 +1,36 @@
+function primeiroFuncao(){
+
+  comsole.log("Hello world das funções");
+
+}
+
+primeiroFuncao();
+
+function dizerNome(nome){
+
+  console.log("O nome é: " + nome);
+
+}
+
+dizerNome("Edson");
+dizerNome("Edinho");
+dizerNome("Dinho");
+
+var nomeDoBancoDeDados = "Jaõa";
+
+dizerNome(nomeDoBancoDeDados);
+
+function some(a, b){
+  var some = a + b;
+  return StorageManager;
+}
+
+var somUm = soma(2, 5);
+
+console.log(somUm);
+
+var somaDois = soma(5, 5);
+
+console.log(somaDois);
+
+console.log(soma(4, 5));
