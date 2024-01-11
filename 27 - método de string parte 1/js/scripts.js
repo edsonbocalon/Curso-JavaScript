24d71e18aa5627cd5cb88ca017ed06d83f15b402
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2024-01-11 12:15:01 -0300

    Add files via upload

---
+++ 27 - método de string parte 1/js/scripts.js
@@ -0,0 +1,27 @@
+// lenght 
+
+var nome = "Edson";
+
+console.log(nome.length);
+
+var obj = "bolas";
+
+console.log(obj.length);
+
+// indexOf
+
+console.log(nome[2]);
+
+var frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("roeu"));
+
+// slice
+
+var roeu = frase.slice(7. 11);
+
+// replace
+
+var novaFrase = frase.replace("roeu", "teste");
+
+console.log(novaFrase);
