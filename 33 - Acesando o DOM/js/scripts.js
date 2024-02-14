e734737ec63cf0a5ba7cf0d312a3de30387ee3f5
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2024-02-14 09:00:01 -0300

    Add files via upload

---
+++ 33 - Acesando o DOM/js/scripts.js
@@ -0,0 +1,18 @@
+// tag
+var titulo = document.getElementsByTagName('h1')[0];
+
+console.log(titulo);
+
+var lis = document.getElementsByTagName('li');
+
+console.log(lis[3]);
+
+// id
+var paragrafo = document.getElementsById('paragrafo');
+
+console.log(paragrafo);
+
+// class
+var itensDaLista = document.getElementsByClassName('item');
+
+console.log(itensDaLista);
